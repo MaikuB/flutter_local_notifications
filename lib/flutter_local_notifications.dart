1b784e559cf475409960a00d72ddb2f849f8121a
Javier Lecuona <javierlecuona@gmail.com>
2018-05-11 04:26:03 -0300

    Asserts and toMap changes to Time class
    Added support for IOS

--- lib/flutter_local_notifications.dart
+++ lib/flutter_local_notifications.dart
@@ -15,13 +15,19 @@ class Time {
   final int minute;
   final int second;
 
-  Time([this.hour = 0, this.minute = 0, this.second = 0]);
+  Time([this.hour = 0, this.minute = 0, this.second = 0]) {
+    assert(this.hour >= 0 && this.hour < 24);
+    assert(this.minute >= 0 && this.minute < 60);
+    assert(this.second >= 0 && this.second < 60);
+  }
 
-  Map<String, int> get serialized => {
-    "hour": hour,
-    "minute": minute,
-    "second": second,
-  };
+  Map<String, int> toMap() {
+    return <String, int> {
+      "hour": hour,
+      "minute": minute,
+      "second": second,
+    };
+  }
 }
 
 class FlutterLocalNotificationsPlugin {
@@ -125,7 +131,7 @@ class FlutterLocalNotificationsPlugin {
       'body': body,
       'calledAt': new DateTime.now().millisecondsSinceEpoch,
       'repeatInterval': RepeatInterval.Daily.index,
-      'repeatTime': notificationTime.serialized,
+      'repeatTime': notificationTime.toMap(),
       'platformSpecifics': serializedPlatformSpecifics,
       'payload': payload ?? ''
     });
