fbeef142282c82288d868db59b1c7b3918d4b8ce
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2018-09-26 23:36:05 +1000

    update docs for 0.3.8 release

--- lib/src/notification_app_launch_details.dart
+++ lib/src/notification_app_launch_details.dart
@@ -1,7 +1,10 @@
 part of flutter_local_notifications;
 
 class NotificationAppLaunchDetails {
+  /// Indicates if the app was launched via notification
   final bool didNotificationLaunchApp;
+
+  /// The payload of the notification that launched the app
   final String payload;
 
   const NotificationAppLaunchDetails(
