5f5f73973d1cca5f5e27b3909bbef704d5ad2e4c
JeffScaturro <jeffrey.scaturro@gmail.com>
2019-01-30 20:55:46 -0400

    [androidx] update example app and readme

--- android/src/main/java/com/dexterous/flutterlocalnotifications/NotificationService.java
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/NotificationService.java
@@ -2,7 +2,7 @@
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
 import android.util.Log;
 
