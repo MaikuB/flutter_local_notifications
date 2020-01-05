732c55e4cb5f854ac1adaaf97c932f7de8ce2a71
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-01-05 19:25:48 +1100

    remove method channel implementation

--- flutter_local_notifications_platform_interface/lib/flutter_local_notifications_platform_interface.dart
+++ flutter_local_notifications_platform_interface/lib/flutter_local_notifications_platform_interface.dart
@@ -1,13 +1,26 @@
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'src/notification_app_launch_details.dart';
+import 'src/platform_notification_details.dart';
 
 export 'src/notification_app_launch_details.dart';
+export 'src/platform_notification_details.dart';
 
 abstract class FlutterLocalNotificationsPlatform extends PlatformInterface {
   FlutterLocalNotificationsPlatform() : super(token: _token);
 
+  static FlutterLocalNotificationsPlatform _instance;
+
   static final Object _token = Object();
+
+  static FlutterLocalNotificationsPlatform get instance => _instance;
+
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [FlutterLocalNotificationsPlatform] when they register themselves.
+  static set instance(FlutterLocalNotificationsPlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
 
   /// Returns info on if a notification had been used to launch the application.
   Future<NotificationAppLaunchDetails> getNotificationAppLaunchDetails() async {
@@ -16,7 +29,9 @@ abstract class FlutterLocalNotificationsPlatform extends PlatformInterface {
   }
 
   /// Show a notification with an optional payload that will be passed back to the app when a notification is tapped on.
-  Future<void> show(int id, String title, String body, {String payload}) async {
+  Future<void> show(int id, String title, String body,
+      PlatformNotificationDetails notificationDetails,
+      {String payload}) async {
     throw UnimplementedError('show() has not been implemented');
   }
 
