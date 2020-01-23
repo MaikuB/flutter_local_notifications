ea9f761577edfd3b66ba9bca8b4e3ac159449a53
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-01-23 21:50:30 +1100

    tweaks to id validation to used named ArgumentError constructors

--- flutter_local_notifications/lib/src/helpers.dart
+++ flutter_local_notifications/lib/src/helpers.dart
@@ -2,11 +2,11 @@
 /// Ensures IDs are valid 32-bit integers.
 void validateId(int id) {
   if (id == null) {
-    throw ArgumentError('id must not be null');
+    throw ArgumentError.notNull('id');
   }
 
   if (id > 0x7FFFFFFF || id < -0x80000000) {
-    throw ArgumentError(
-        'id must fit within the size of a 32-bit integer i.e. in the range [-2^31, 2^31 - 1]');
+    throw ArgumentError.value(id, 'id',
+        'must fit within the size of a 32-bit integer i.e. in the range [-2^31, 2^31 - 1]');
   }
 }
