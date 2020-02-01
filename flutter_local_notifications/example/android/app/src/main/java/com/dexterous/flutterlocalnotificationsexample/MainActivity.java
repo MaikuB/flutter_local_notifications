bfb4e37d845ad90d53b978ba2a1bdb7cbaab7f8f
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-02-01 14:50:37 +1100

    add e2e test

--- flutter_local_notifications/example/android/app/src/main/java/com/dexterous/flutterlocalnotificationsexample/MainActivity.java
+++ flutter_local_notifications/example/android/app/src/main/java/com/dexterous/flutterlocalnotificationsexample/MainActivity.java
@@ -33,11 +33,4 @@ public class MainActivity extends FlutterActivity {
                     }
                 });
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-
-    }
 }
