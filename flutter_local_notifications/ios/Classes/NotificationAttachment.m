ba6f95dea57c9c8a739e328433f6b8d4c37dd88a
Pavel Sipaylo <pavel.sipaylo@gmail.com>
2020-03-21 02:24:02 +0300

    Clean up

--- flutter_local_notifications/ios/Classes/NotificationAttachment.m
+++ flutter_local_notifications/ios/Classes/NotificationAttachment.m
@@ -1,10 +1,3 @@
-//
-//  NotificationAttachment.m
-//  flutter_local_notifications
-//
-//  Created by Pavel Sipaylo on 3/20/20.
-//
-
 #import "NotificationAttachment.h"
 
 @implementation NotificationAttachment
