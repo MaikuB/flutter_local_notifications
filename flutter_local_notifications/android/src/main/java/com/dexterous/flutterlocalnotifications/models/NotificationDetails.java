2874a523d3ef02e99733d7c18607c76b80296cfb
zaaru <vgerash@gmail.com>
2020-04-07 12:06:08 +0300

    add channelCreate(Boolean) in NotificationDetails for init channel only;

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/models/NotificationDetails.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/models/NotificationDetails.java
@@ -103,6 +103,8 @@ public class NotificationDetails {
     private static final String TIMEOUT_AFTER = "timeoutAfter";
     private static final String SHOW_WHEN = "showWhen";
 
+    private static final String CHANNEL_CREATE = "channelCreate";
+
 
     public Integer id;
     public String title;
@@ -151,6 +153,7 @@ public class NotificationDetails {
     public Long timeoutAfter;
     public String category;
     public Boolean showWhen;
+    public Boolean channelCreate;
 
 
 
@@ -292,6 +295,7 @@ public class NotificationDetails {
             notificationDetails.importance = (Integer) platformChannelSpecifics.get(IMPORTANCE);
             notificationDetails.channelShowBadge = (Boolean) platformChannelSpecifics.get(CHANNEL_SHOW_BADGE);
             notificationDetails.channelAction = NotificationChannelAction.values()[(Integer) platformChannelSpecifics.get(CHANNEL_ACTION)];
+            notificationDetails.channelCreate = (Boolean) platformChannelSpecifics.get(CHANNEL_CREATE);
         }
     }
 
