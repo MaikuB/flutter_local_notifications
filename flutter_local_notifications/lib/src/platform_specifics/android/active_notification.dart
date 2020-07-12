eb8c9f3c14f145d7f26c9462f8a15dcffafba5a1
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-07-12 11:15:01 +1000

    amend API docs

--- flutter_local_notifications/lib/src/platform_specifics/android/active_notification.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/active_notification.dart
@@ -9,9 +9,9 @@ class ActiveNotification {
   /// The notification's id.
   final int id;
 
-  /// The notification channel's id
+  /// The notification channel's id.
   ///
-  /// Returned only on Android 8.0+
+  /// Returned only on Android 8.0+.
   final String channelId;
 
   /// The notification's title.
