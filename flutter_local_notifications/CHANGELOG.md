cb6bcf8a8335caa81861db929c38c06b40dc0dc0
Le Liam <nghenglim@gmail.com>
2020-10-15 16:56:56 +0800

    [flutter_local_notifications] Support notification reschedule for android during restart / quickboot (#850)
    * able to reschedule on android restart
    
    * enable notification reschedule for android quickboot
    
    * code style
    
    * bump plugin and add changelog entry to 2.0.1
    
    * change wording in changelog entry for 2.0.1
    
    Co-authored-by: nghenglim <me@leliam.com>
    Co-authored-by: Michael Bui <25263378+MaikuB@users.noreply.github.com>

--- flutter_local_notifications/CHANGELOG.md
+++ flutter_local_notifications/CHANGELOG.md
@@ -1,3 +1,7 @@
+# [2.0.1]
+
+* [Android] updated plugin and steps in the readme to ensure notifications remain scheduled after a HTC device restarts. Thanks to the PR from [Le Liam](https://github.com/nghenglim)
+
 # [2.0.0+1]
 
 * Fixed code snippet in readme around initialisation and configuring the `onDidReceiveLocalNotification` callback specific to iOS. Thanks to the PR from [Mike Truso](https://github.com/mftruso)
--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationBootReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationBootReceiver.java
@@ -11,7 +11,9 @@ public class ScheduledNotificationBootReceiver extends BroadcastReceiver
         String action = intent.getAction();
         if (action != null) {
             if (action.equals(android.content.Intent.ACTION_BOOT_COMPLETED)
-                    || action.equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+                    || action.equals(Intent.ACTION_MY_PACKAGE_REPLACED)
+                    || action.equals("android.intent.action.QUICKBOOT_POWERON")
+                    || action.equals("com.htc.intent.action.QUICKBOOT_POWERON")) {
                 FlutterLocalNotificationsPlugin.rescheduleNotifications(context);
             }
         }
