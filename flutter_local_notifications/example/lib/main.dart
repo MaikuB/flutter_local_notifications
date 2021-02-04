d8adf19f2e951fa0eb1acb941a6be5e272c4e542
Shapovalova Vera <47853911+VAShapovalova@users.noreply.github.com>
2021-02-04 15:54:12 +0700

    update example

--- flutter_local_notifications/example/lib/main.dart
+++ flutter_local_notifications/example/lib/main.dart
@@ -1567,44 +1567,45 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<Widget> _getNotificationChannelsDialogContent() async {
-    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-    if (!(androidInfo.version.sdkInt >= 26)) {
-      return const Text(
-        '"getActiveNotifications" is available only for Android 8.0 or newer',
-      );
-    }
-
     try {
-      final List<AndroidNotificationChannelOutputInfo> channels =
+      final List<AndroidNotificationChannel> channels =
           await flutterLocalNotificationsPlugin
               .resolvePlatformSpecificImplementation<
                   AndroidFlutterLocalNotificationsPlugin>()
               ?.getNotificationChannels();
 
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text(
-            'Active Notifications',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const Divider(color: Colors.black),
-          if (channels.isEmpty) const Text('No notification channels'),
-          if (channels.isNotEmpty)
-            for (AndroidNotificationChannelOutputInfo channel in channels)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('id: ${channel.id}\n'
-                      'channelId: ${channel.name}\n'
-                      'title: ${channel.importance}\n'),
-                  const Divider(color: Colors.black),
-                ],
+      return Container(
+          width: double.maxFinite,
+          child: ListView(
+            children: <Widget>[
+              const Text(
+                'Notifications Channels',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-        ],
-      );
+              const Divider(color: Colors.black),
+              if (channels?.isEmpty) const Text('No notification channels'),
+              if (channels.isNotEmpty)
+                for (AndroidNotificationChannel channel in channels)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('id: ${channel.id}\n'
+                          'name: ${channel.name}\n'
+                          'description: ${channel.description}\n'
+                          'groupId: ${channel.groupId}\n'
+                          'importance: ${channel.importance.value}\n'
+                          'playSound: ${channel.playSound}\n'
+                          'sound: ${channel.sound.sound}\n'
+                          'enableVibration: ${channel.enableVibration}\n'
+                          'vibrationPattern: ${channel.vibrationPattern}\n'
+                          'showBadge: ${channel.showBadge}\n'
+                          'enableLights: ${channel.enableLights}\n'
+                          'ledColor: ${channel.ledColor}\n'),
+                      const Divider(color: Colors.black),
+                    ],
+                  ),
+            ],
+          ));
     } on PlatformException catch (error) {
       return Text(
         'Error calling "getNotificationChannels"\n'
