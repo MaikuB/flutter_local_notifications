9ebe1c980865c63e8c2255cb9cfd3a276fac25ce
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2021-07-26 21:19:11 +1000

    change AndroidBitmap and its implementations to make use of generics

--- flutter_local_notifications/lib/src/platform_specifics/android/bitmap.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/bitmap.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:flutter_local_notifications/src/platform_specifics/android/enums.dart';
+import 'enums.dart';
 
 /// Represents a bitmap on Android.
-abstract class AndroidBitmap {
+abstract class AndroidBitmap<T> {
   /// The location of the bitmap.
-  Object get data;
+  T get data;
 
   /// The subclass source type
   AndroidBitmapSource get source;
@@ -14,7 +14,7 @@ abstract class AndroidBitmap {
 
 /// Represents a drawable resource belonging to the Android application that
 /// should be used as a bitmap on Android.
-class DrawableResourceAndroidBitmap implements AndroidBitmap {
+class DrawableResourceAndroidBitmap implements AndroidBitmap<String> {
   /// Constructs an instance of [DrawableResourceAndroidBitmap].
   const DrawableResourceAndroidBitmap(this._bitmap);
 
@@ -24,14 +24,14 @@ class DrawableResourceAndroidBitmap implements AndroidBitmap {
   ///
   /// For example if the drawable resource is located at `res/drawable/app_icon.png`, the bitmap should be `app_icon`
   @override
-  Object get data => _bitmap;
+  String get data => _bitmap;
 
   @override
   AndroidBitmapSource get source => AndroidBitmapSource.drawable;
 }
 
 /// Represents a file path that should be used for a bitmap on Android.
-class FilePathAndroidBitmap implements AndroidBitmap {
+class FilePathAndroidBitmap implements AndroidBitmap<String> {
   /// Constructs an instance of [FilePathAndroidBitmap].
   const FilePathAndroidBitmap(this._bitmap);
 
@@ -40,24 +40,26 @@ class FilePathAndroidBitmap implements AndroidBitmap {
   /// A file path on the Android device that refers to the location of the
   /// bitmap.
   @override
-  Object get data => _bitmap;
+  String get data => _bitmap;
 
   @override
   AndroidBitmapSource get source => AndroidBitmapSource.filePath;
 }
 
 /// Represents a base64 encoded AndroidBitmap.
-class ByteArrayAndroidBitmap implements AndroidBitmap {
+class ByteArrayAndroidBitmap implements AndroidBitmap<Uint8List> {
   /// Constructs an instance of [ByteArrayAndroidBitmap].
   const ByteArrayAndroidBitmap(this._bitmap);
 
-  factory ByteArrayAndroidBitmap.fromBase64String(String base64Image) => ByteArrayAndroidBitmap(base64Decode(base64Image));
+  /// Constructs an instance of [ByteArrayAndroidBitmap] from a base64 string.
+  factory ByteArrayAndroidBitmap.fromBase64String(String base64Image) =>
+      ByteArrayAndroidBitmap(base64Decode(base64Image));
 
   final Uint8List _bitmap;
 
   /// A base64 encoded Bitmap string.
   @override
-  Object get data => _bitmap;
+  Uint8List get data => _bitmap;
 
   @override
   AndroidBitmapSource get source => AndroidBitmapSource.byteArray;
