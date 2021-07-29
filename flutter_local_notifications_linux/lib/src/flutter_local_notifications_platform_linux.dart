e69b18d2a66be8b564ccc43be56d6e15cb18586a
proninyaroslav <proninyaroslav@gmail.com>
2021-07-29 11:57:00 +0300

    [Linux] Stub all plugin methods

--- flutter_local_notifications_linux/lib/src/flutter_local_notifications_platform_linux.dart
+++ flutter_local_notifications_linux/lib/src/flutter_local_notifications_platform_linux.dart
@@ -1,43 +1,25 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications_platform_interface/flutter_local_notifications_platform_interface.dart';
 
-import 'flutter_local_notifications_platform_linux.dart';
 import 'model/capabilities.dart';
 import 'model/initialization_settings.dart';
 import 'model/notification_details.dart';
-import 'notifications_manager.dart';
 
-/// Linux implementation of the local notifications plugin.
-class LinuxFlutterLocalNotificationsPlugin
-    extends FlutterLocalNotificationsPlatformLinux {
-  /// Constructs an instance of [LinuxNotificationDetails].
-  LinuxFlutterLocalNotificationsPlugin()
-      : _manager = LinuxNotificationManager();
+export 'package:flutter_local_notifications_platform_interface/flutter_local_notifications_platform_interface.dart';
 
-  /// Constructs an instance of [LinuxNotificationDetails]
-  /// with the give [manager].
-  @visibleForTesting
-  LinuxFlutterLocalNotificationsPlugin.private(
-    LinuxNotificationManager manager,
-  ) : _manager = manager;
-
-  final LinuxNotificationManager _manager;
-
+/// The interface that all implementations of flutter_local_notifications_linux
+/// must implement.
+abstract class FlutterLocalNotificationsPlatformLinux
+    extends FlutterLocalNotificationsPlatform {
   /// Initializes the plugin.
   ///
   /// Call this method on application before using the plugin further.
   /// This should only be done once. When a notification created by this plugin
   /// was used to launch the app, calling `initialize` is what will trigger to
   /// the `onSelectNotification` callback to be fire.
-  @override
   Future<bool?> initialize(
     LinuxInitializationSettings initializationSettings, {
     SelectNotificationCallback? onSelectNotification,
-  }) async {
-    await _manager.initialize(
-      initializationSettings,
-      onSelectNotification: onSelectNotification,
-    );
-  }
+  });
 
   /// Show a notification with an optional payload that will be passed back to
   /// the app when a notification is tapped on.
@@ -48,32 +30,12 @@ class LinuxFlutterLocalNotificationsPlugin
     String? body, {
     LinuxNotificationDetails? notificationDetails,
     String? payload,
-  }) {
-    validateId(id);
-    return _manager.show(
-      id,
-      title,
-      body,
-      details: notificationDetails,
-      payload: payload,
-    );
-  }
-
-  @override
-  Future<void> cancel(int id) {
-    validateId(id);
-    return _manager.cancel(id);
-  }
-
-  @override
-  Future<void> cancelAll() => _manager.cancelAll();
+  });
 
   /// Returns the system notification server capabilities.
   /// Some functionality may not be implemented by the notification server,
   /// conforming clients should check if it is available before using it.
-  @override
-  Future<LinuxServerCapabilities> getCapabilities() =>
-      _manager.getCapabilities();
+  Future<LinuxServerCapabilities> getCapabilities();
 
   /// Returns a [Map] with the specified notification id as the key
   /// and the id, assigned by the system, as the value.
@@ -82,6 +44,5 @@ class LinuxFlutterLocalNotificationsPlugin
   /// so it's undesirable to save it to persistable storage without any
   /// invalidation/update. For more information, please see
   /// Desktop Notifications Specification https://developer.gnome.org/notification-spec/#basic-design
-  @override
-  Future<Map<int, int>> getSystemIdMap() => _manager.getSystemIdMap();
+  Future<Map<int, int>> getSystemIdMap();
 }
--- flutter_local_notifications_linux/lib/src/flutter_local_notifications_stub.dart
+++ flutter_local_notifications_linux/lib/src/flutter_local_notifications_stub.dart
@@ -1,4 +1,7 @@
-import 'package:flutter_local_notifications_platform_interface/flutter_local_notifications_platform_interface.dart';
+import 'flutter_local_notifications_platform_linux.dart';
+import 'model/capabilities.dart';
+import 'model/initialization_settings.dart';
+import 'model/notification_details.dart';
 
 /// A stub implementation to satisfy compilation of multi-platform packages that
 /// depend on flutter_local_notifications_linux.
@@ -10,8 +13,47 @@ import 'package:flutter_local_notifications_platform_interface/flutter_local_not
 /// flutter_local_notifications_linux, not just at
 /// the pubspec level but the code level.
 class LinuxFlutterLocalNotificationsPlugin
-    extends FlutterLocalNotificationsPlatform {
+    extends FlutterLocalNotificationsPlatformLinux {
   /// Errors on attempted instantiation of the stub. It exists only to satisfy
   /// compile-time dependencies, and should never actually be created.
   LinuxFlutterLocalNotificationsPlugin() : assert(false);
+
+  /// Errors on attempted calling of the stub. It exists only to satisfy
+  /// compile-time dependencies, and should never actually be called.
+  @override
+  Future<bool?> initialize(
+    LinuxInitializationSettings initializationSettings, {
+    SelectNotificationCallback? onSelectNotification,
+  }) async {
+    assert(false);
+  }
+
+  /// Errors on attempted calling of the stub. It exists only to satisfy
+  /// compile-time dependencies, and should never actually be called.
+  @override
+  Future<void> show(
+    int id,
+    String? title,
+    String? body, {
+    LinuxNotificationDetails? notificationDetails,
+    String? payload,
+  }) async {
+    assert(false);
+  }
+
+  /// Errors on attempted calling of the stub. It exists only to satisfy
+  /// compile-time dependencies, and should never actually be called.
+  @override
+  Future<LinuxServerCapabilities> getCapabilities() async {
+    assert(false);
+    throw UnimplementedError();
+  }
+
+  /// Errors on attempted calling of the stub. It exists only to satisfy
+  /// compile-time dependencies, and should never actually be called.
+  @override
+  Future<Map<int, int>> getSystemIdMap() async {
+    assert(false);
+    throw UnimplementedError();
+  }
 }
