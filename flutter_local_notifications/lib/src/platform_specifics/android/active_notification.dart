e72a9a6222ab7296623ddefe7fbd77815e71119a
roman.donchenko <drstranges@gmail.com>
2021-11-04 19:58:55 +0200

    Add groupKey field to ActiveNotification

--- flutter_local_notifications/lib/src/platform_specifics/android/active_notification.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/active_notification.dart
@@ -4,6 +4,7 @@ class ActiveNotification {
   /// Constructs an instance of [ActiveNotification].
   const ActiveNotification(
     this.id,
+    this.groupKey,
     this.channelId,
     this.title,
     this.body,
@@ -11,6 +12,9 @@ class ActiveNotification {
 
   /// The notification's id.
   final int id;
+
+  /// The notification's group.
+  final String? groupKey;
 
   /// The notification channel's id.
   ///
