ed2036cc20cfa8ac60f55f8b591e7f938bed7c70
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2021-11-19 08:08:22 +1100

    Revert "[flutter_local_notifications] use executor service to commit to Android shared prefs in background and pass method channel result once committed (#1386)"
    This reverts commit 20d611a4909d350aefe6391cb1428cb9acc35470.

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
@@ -32,7 +32,7 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
       notificationManager.notify(notificationId, notification);
       boolean repeat = intent.getBooleanExtra("repeat", false);
       if (!repeat) {
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId, null);
+        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
       }
     } else {
       Gson gson = FlutterLocalNotificationsPlugin.buildGson();
@@ -40,17 +40,16 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
       NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
       FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
       if (notificationDetails.scheduledNotificationRepeatFrequency != null) {
-        FlutterLocalNotificationsPlugin.zonedScheduleNextNotification(
-            context, notificationDetails, null);
+        FlutterLocalNotificationsPlugin.zonedScheduleNextNotification(context, notificationDetails);
       } else if (notificationDetails.matchDateTimeComponents != null) {
         FlutterLocalNotificationsPlugin.zonedScheduleNextNotificationMatchingDateComponents(
-            context, notificationDetails, null);
+            context, notificationDetails);
       } else if (notificationDetails.repeatInterval != null) {
         FlutterLocalNotificationsPlugin.scheduleNextRepeatingNotification(
-            context, notificationDetails, null);
+            context, notificationDetails);
       } else {
         FlutterLocalNotificationsPlugin.removeNotificationFromCache(
-            context, notificationDetails.id, null);
+            context, notificationDetails.id);
       }
     }
   }
