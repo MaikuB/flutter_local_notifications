5eca67d36a3c9277a19e68130587fe83ea221322
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2022-02-11 21:03:24 +1100

    fix grammar and formatting issues in API docs for colorized property

--- flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
@@ -283,7 +283,13 @@ class AndroidNotificationDetails {
   /// the id to uniquely identify alarms.
   final String? tag;
 
-  /// Specify coloring background should be enabled, if false, color will be applied to app icon.
-  /// For most styles, the coloring will only be applied if the notification is for a foreground service notification.
+  /// Specifies if the [color] should be used as the background colour of the
+  /// notification.
+  ///
+  /// Set `true` to enable this. If `false` then the [color] will be
+  /// applied to the app icon.
+  ///
+  /// For most styles, the colouring will only be applied if the notification is
+  /// for a foreground service notification.
   final bool colorized;
 }
