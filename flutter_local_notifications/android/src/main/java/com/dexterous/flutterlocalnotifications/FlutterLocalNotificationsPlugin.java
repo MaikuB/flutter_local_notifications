d8db25a76ca9afe9b9529a1331769d2c1d13b2c5
Simon Ser <contact@emersion.fr>
2022-03-25 17:27:26 +0100

    Add getActiveNotificationMessagingStyle
    This allows fetching an active notification's MessagingStyleInformation.
    A typical use-case is appending a new Message to a MessagingStyle
    when receiving a push message.

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -101,6 +101,7 @@ public class FlutterLocalNotificationsPlugin
   private static final String CREATE_NOTIFICATION_CHANNEL_METHOD = "createNotificationChannel";
   private static final String DELETE_NOTIFICATION_CHANNEL_METHOD = "deleteNotificationChannel";
   private static final String GET_ACTIVE_NOTIFICATIONS_METHOD = "getActiveNotifications";
+  private static final String GET_ACTIVE_NOTIFICATION_MESSAGING_STYLE_METHOD = "getActiveNotificationMessagingStyle";
   private static final String GET_NOTIFICATION_CHANNELS_METHOD = "getNotificationChannels";
   private static final String START_FOREGROUND_SERVICE = "startForegroundService";
   private static final String STOP_FOREGROUND_SERVICE = "stopForegroundService";
@@ -126,6 +127,8 @@ public class FlutterLocalNotificationsPlugin
       "GET_ACTIVE_NOTIFICATIONS_ERROR_CODE";
   private static final String GET_ACTIVE_NOTIFICATIONS_ERROR_MESSAGE =
       "Android version must be 6.0 or newer to use getActiveNotifications";
+  private static final String GET_ACTIVE_MESSAGING_STYLE_ERROR_CODE =
+      "GET_ACTIVE_MESSAGING_STYLE_ERROR_CODE";
   private static final String GET_NOTIFICATION_CHANNELS_ERROR_CODE =
       "GET_NOTIFICATION_CHANNELS_ERROR_CODE";
   private static final String INVALID_LED_DETAILS_ERROR_MESSAGE =
@@ -1270,6 +1273,9 @@ public class FlutterLocalNotificationsPlugin
       case GET_ACTIVE_NOTIFICATIONS_METHOD:
         getActiveNotifications(result);
         break;
+      case GET_ACTIVE_NOTIFICATION_MESSAGING_STYLE_METHOD:
+        getActiveNotificationMessagingStyle(call, result);
+        break;
       case GET_NOTIFICATION_CHANNELS_METHOD:
         getNotificationChannels(result);
         break;
@@ -1605,6 +1611,103 @@ public class FlutterLocalNotificationsPlugin
     } catch (Throwable e) {
       result.error(GET_ACTIVE_NOTIFICATIONS_ERROR_CODE, e.getMessage(), e.getStackTrace());
     }
+  }
+
+  private void getActiveNotificationMessagingStyle(MethodCall call, Result result) {
+    if (VERSION.SDK_INT < VERSION_CODES.M) {
+      result.error(
+          GET_ACTIVE_MESSAGING_STYLE_ERROR_CODE,
+          "Android version must be 6.0 or newer to use getActiveNotificationMessagingStyle", null);
+      return;
+    }
+    NotificationManager notificationManager =
+        (NotificationManager) applicationContext.getSystemService(Context.NOTIFICATION_SERVICE);
+    try {
+      Map<String, Object> arguments = call.arguments();
+      int id = (int) arguments.get("id");
+      String tag = (String) arguments.get("tag");
+
+      StatusBarNotification[] activeNotifications = notificationManager.getActiveNotifications();
+      Notification notification = null;
+      for (StatusBarNotification activeNotification : activeNotifications) {
+        if (activeNotification.getId() == id && (tag == null || activeNotification.getTag() == tag)) {
+          notification = activeNotification.getNotification();
+          break;
+        }
+      }
+
+      if (notification == null) {
+        result.success(null);
+        return;
+      }
+
+      NotificationCompat.MessagingStyle messagingStyle = NotificationCompat.MessagingStyle.extractMessagingStyleFromNotification(notification);
+      if (messagingStyle == null) {
+        result.success(null);
+        return;
+      }
+
+      HashMap<String, Object> stylePayload = new HashMap<>();
+      stylePayload.put("groupConversation", messagingStyle.isGroupConversation());
+      stylePayload.put("person", describePerson(messagingStyle.getUser()));
+      stylePayload.put("conversationTitle", messagingStyle.getConversationTitle());
+
+      List<Map<String, Object>> messagesPayload = new ArrayList<>();
+      for (NotificationCompat.MessagingStyle.Message msg : messagingStyle.getMessages()) {
+        Map<String, Object> msgPayload = new HashMap<>();
+        msgPayload.put("text", msg.getText());
+        msgPayload.put("timestamp", msg.getTimestamp());
+        msgPayload.put("person", describePerson(msg.getPerson()));
+        messagesPayload.add(msgPayload);
+      }
+      stylePayload.put("messages", messagesPayload);
+
+      result.success(stylePayload);
+    } catch (Throwable e) {
+      result.error(GET_ACTIVE_MESSAGING_STYLE_ERROR_CODE, e.getMessage(), e.getStackTrace());
+    }
+  }
+
+  private Map<String, Object> describePerson(Person person) {
+    if (person == null) {
+      return null;
+    }
+    Map<String, Object> payload = new HashMap<>();
+    payload.put("key", person.getKey());
+    payload.put("name", person.getName());
+    payload.put("uri", person.getUri());
+    payload.put("bot", person.isBot());
+    payload.put("important", person.isImportant());
+    payload.put("icon", describeIcon(person.getIcon()));
+    return payload;
+  }
+
+  private Map<String, Object> describeIcon(IconCompat icon) {
+    if (icon == null) {
+      return null;
+    }
+    IconSource source;
+    Object data;
+    switch (icon.getType()) {
+      case IconCompat.TYPE_RESOURCE:
+        source = IconSource.DrawableResource;
+        int resId = icon.getResId();
+        Context context = applicationContext;
+        assert(context.getResources().getResourceTypeName(resId).equals(DRAWABLE));
+        assert(context.getResources().getResourcePackageName(resId).equals(context.getPackageName()));
+        data = context.getResources().getResourceEntryName(resId);
+        break;
+      case IconCompat.TYPE_URI:
+        source = IconSource.ContentUri;
+        data = icon.getUri().toString();
+        break;
+      default:
+        return null;
+    }
+    Map<String, Object> payload = new HashMap<>();
+    payload.put("source", source.ordinal());
+    payload.put("data", data);
+    return payload;
   }
 
   private void getNotificationChannels(Result result) {
