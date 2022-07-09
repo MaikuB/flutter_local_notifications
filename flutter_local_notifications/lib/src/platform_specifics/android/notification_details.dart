927da617ee415edd9f5a1a3b322d4b477d48dd8a
Katsuya Kato <crossbridge.kato@gmail.com>
2022-07-09 11:12:00 +0900

    update API doc

--- flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
@@ -294,6 +294,10 @@ class AndroidNotificationDetails {
   final bool colorized;
 
   /// Set custom notification count.
-  /// Only displayed in compatible home app.
+  ///
+  /// For Android 8.0 or newer,
+  /// Numbers are only displayed if the launcher application supports the display of badges and numbers.
+  /// If not supported, this value is ignored.
+  /// See https://developer.android.com/training/notify-user/badges#set_custom_notification_count
   final int? number;
 }
