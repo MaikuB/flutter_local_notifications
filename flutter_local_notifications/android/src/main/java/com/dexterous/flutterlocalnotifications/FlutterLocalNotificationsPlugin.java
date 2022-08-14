78da1815993edd8924d83a2c0950c44c21879075
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2022-08-14 19:53:06 +1000

    fix merge issue

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -1441,28 +1441,17 @@ public class FlutterLocalNotificationsPlugin
 
   private void getNotificationAppLaunchDetails(Result result) {
     Map<String, Object> notificationAppLaunchDetails = new HashMap<>();
-    Boolean notificationLaunchedApp =
-        mainActivity != null
-            && (SELECT_NOTIFICATION.equals(mainActivity.getIntent().getAction())
-                || SELECT_FOREGROUND_NOTIFICATION_ACTION.equals(
-                    mainActivity.getIntent().getAction()))
-            && !launchedActivityFromHistory(mainActivity.getIntent());
-    notificationAppLaunchDetails.put(NOTIFICATION_LAUNCHED_APP, notificationLaunchedApp);
-    if (notificationLaunchedApp) {
-      notificationAppLaunchDetails.put(
-          "notificationResponse", extractNotificationResponseMap(launchIntent));
     Boolean notificationLaunchedApp = false;
     if (mainActivity != null) {
       Intent launchIntent = mainActivity.getIntent();
       notificationLaunchedApp =
-              launchIntent != null
-                      && (SELECT_NOTIFICATION.equals(launchIntent.getAction())
-                      || SELECT_FOREGROUND_NOTIFICATION_ACTION.equals(
-                      launchIntent.getAction()))
-                      && !launchedActivityFromHistory(launchIntent);
+          launchIntent != null
+              && (SELECT_NOTIFICATION.equals(launchIntent.getAction())
+                  || SELECT_FOREGROUND_NOTIFICATION_ACTION.equals(launchIntent.getAction()))
+              && !launchedActivityFromHistory(launchIntent);
       if (notificationLaunchedApp) {
         notificationAppLaunchDetails.put(
-                "notificationResponse", extractNotificationResponseMap(launchIntent));
+            "notificationResponse", extractNotificationResponseMap(launchIntent));
       }
     }
 
