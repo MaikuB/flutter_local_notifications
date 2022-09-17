3dfd6f935cb8eaaf939e155136f863ed40202e4e
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2022-09-17 23:03:01 +1000

    fix android deprecation warnings around intent APIs

--- flutter_local_notifications/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_local_notifications/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flutter_local_notifications
 import flutter_native_timezone
 import path_provider_macos
 import shared_preferences_macos
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
@@ -18,5 +17,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
