1824d5b0dfd7f1ef6afd890b99eecd335edeffde
Rexios <rexios@rexios.dev>
2022-11-23 17:01:23 -0500

    Revert "Reverted timezone plugin change"
    This reverts commit e8761925f6c64829f597caeec316e62454e9a63e.

--- flutter_local_notifications/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_local_notifications/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,12 @@ import Foundation
 
 import device_info_plus
 import flutter_local_notifications
-import flutter_native_timezone
+import flutter_timezone
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
