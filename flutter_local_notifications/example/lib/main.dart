77a2d074bf80e3ac6b864f094f28eada82aa5e10
dowen.den <dowen.den@linecorp.com>
2023-07-03 10:22:13 +0800

    feat: remove provisional permission from example app

--- flutter_local_notifications/example/lib/main.dart
+++ flutter_local_notifications/example/lib/main.dart
@@ -310,29 +310,6 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<void> _requestProvisionalPermissions() async {
-    if (Platform.isIOS || Platform.isMacOS) {
-      await flutterLocalNotificationsPlugin
-          .resolvePlatformSpecificImplementation<
-              IOSFlutterLocalNotificationsPlugin>()
-          ?.requestPermissions(
-            alert: true,
-            badge: true,
-            sound: true,
-            provisional: true,
-          );
-      await flutterLocalNotificationsPlugin
-          .resolvePlatformSpecificImplementation<
-              MacOSFlutterLocalNotificationsPlugin>()
-          ?.requestPermissions(
-            alert: true,
-            badge: true,
-            sound: true,
-            provisional: true,
-          );
-    }
-  }
-
   void _configureDidReceiveLocalNotificationSubject() {
     didReceiveLocalNotificationStream.stream
         .listen((ReceivedNotification receivedNotification) async {
@@ -845,10 +822,6 @@ class _HomePageState extends State<HomePage> {
                     PaddedElevatedButton(
                       buttonText: 'Request permission',
                       onPressed: _requestPermissions,
-                    ),
-                    PaddedElevatedButton(
-                      buttonText: 'Request provisional permission',
-                      onPressed: _requestProvisionalPermissions,
                     ),
                     PaddedElevatedButton(
                       buttonText: 'Show notification with subtitle',
