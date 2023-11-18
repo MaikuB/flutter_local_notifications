9d8c793219be4561cfc14942be3daed3dc262f05
aa-euclidk <112929539+aa-euclidk@users.noreply.github.com>
2023-11-18 12:31:18 +0800

    [flutter_local_notifications] added ability to specify silent notification through AndroidNotificationDetails (#2102)
    * setSilent notification based on playSound argument
    
    * Add silent option to AndroidNotificationDetails
    
    * Add test for default AndroidDetails and silent enabled
    
    * Add button with silent AndroidNotificationDetails
    
    * Reorder "silent" field to be consistently after "ongoing"
    
    ---------
    
    Co-authored-by: Michael Bui <25263378+MaikuB@users.noreply.github.com>

--- flutter_local_notifications/example/lib/main.dart
+++ flutter_local_notifications/example/lib/main.dart
@@ -479,9 +479,16 @@ class _HomePageState extends State<HomePage> {
                     },
                   ),
                   PaddedElevatedButton(
-                    buttonText: 'Show notification with no sound',
+                    buttonText: 'Show notification from silent channel',
                     onPressed: () async {
                       await _showNotificationWithNoSound();
+                    },
+                  ),
+                  PaddedElevatedButton(
+                    buttonText:
+                        'Show silent notification from channel with sound',
+                    onPressed: () async {
+                      await _showNotificationSilently();
                     },
                   ),
                   PaddedElevatedButton(
@@ -1354,6 +1361,26 @@ class _HomePageState extends State<HomePage> {
         playSound: false,
         styleInformation: DefaultStyleInformation(true, true));
     const DarwinNotificationDetails darwinNotificationDetails = DarwinNotificationDetails(
+      presentSound: false,
+    );
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: darwinNotificationDetails,
+        macOS: darwinNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(
+        id++, '<b>silent</b> title', '<b>silent</b> body', notificationDetails);
+  }
+
+  Future<void> _showNotificationSilently() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('your channel id', 'your channel name',
+            channelDescription: 'your channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker',
+            silent: true);
+    const DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
       presentSound: false,
     );
     const NotificationDetails notificationDetails = NotificationDetails(
