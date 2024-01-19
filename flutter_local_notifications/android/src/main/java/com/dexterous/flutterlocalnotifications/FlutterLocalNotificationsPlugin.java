2e084517c15373959c88fce650bf9a6935af3544
Ruchi Purohit <30714929+RuchiPurohit@users.noreply.github.com>
2024-01-19 02:53:13 -0800

    [flutter_local_notifications] setupAlarm method: handle ScheduleMode.useAllowWhileIdle() called on null (#2199)
    * Handle ScheduleMode.useAllowWhileIdle() called on a null object reference
    
    * handle null: added scheduleMode = ScheduleMode.exact
    
    * added semicolon
    
    * fixed typo in comments
    
    ---------
    
    Co-authored-by: Michael Bui <25263378+MaikuB@users.noreply.github.com>

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -598,7 +598,7 @@ public class FlutterLocalNotificationsPlugin
     AlarmManager alarmManager = getAlarmManager(context);
     if (notificationDetails.scheduleMode == null) {
       // This is to account for notifications created in older versions prior to allowWhileIdle
-      // being added so the deserialiser.
+      // being added to the deserialiser.
       // Reference to old behaviour:
       // https://github.com/MaikuB/flutter_local_notifications/blob/4b723e750d1371206520b10a122a444c4bba7475/flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java#L569C37-L569C37
       notificationDetails.scheduleMode = ScheduleMode.exactAllowWhileIdle;
@@ -676,7 +676,7 @@ public class FlutterLocalNotificationsPlugin
 
     if (notificationDetails.scheduleMode == null) {
       // This is to account for notifications created in older versions prior to allowWhileIdle
-      // being added so the deserialiser.
+      // being added to the deserialiser.
       // Reference to old behaviour:
       // https://github.com/MaikuB/flutter_local_notifications/blob/4b723e750d1371206520b10a122a444c4bba7475/flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java#L642
       notificationDetails.scheduleMode = ScheduleMode.inexact;
@@ -700,6 +700,15 @@ public class FlutterLocalNotificationsPlugin
       AlarmManager alarmManager,
       long epochMilli,
       PendingIntent pendingIntent) {
+
+    if (notificationDetails.scheduleMode == null) {
+      // This is to account for notifications created in older versions prior to allowWhileIdle
+      // being added to the deserialiser.
+      // Reference to old behaviour:
+      // https://github.com/MaikuB/flutter_local_notifications/blob/master/flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java#L735
+      notificationDetails.scheduleMode = ScheduleMode.exact;
+    }
+
     if (notificationDetails.scheduleMode.useAllowWhileIdle()) {
       setupAllowWhileIdleAlarm(notificationDetails, alarmManager, epochMilli, pendingIntent);
     } else {
