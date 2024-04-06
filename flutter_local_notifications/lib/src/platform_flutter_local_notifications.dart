02472c04c89a6a8df98e9080c09a9958b7a4e57e
Szilard <szilard@szurti.hu>
2024-04-06 14:53:51 +0200

    Update platform_flutter_local_notifications.dart

--- flutter_local_notifications/lib/src/platform_flutter_local_notifications.dart
+++ flutter_local_notifications/lib/src/platform_flutter_local_notifications.dart
@@ -364,7 +364,7 @@ class AndroidFlutterLocalNotificationsPlugin
   /// then the notification that matches both the id and the tag will
   /// be canceled. `tag` has no effect on other platforms.
   @override
-  Future<void> cancel(int id, {String? tag}) async {
+  Future<void> cancel(dynamic id, {String? tag}) async {
     validateId(id);
 
     return _channel.invokeMethod('cancel', <String, Object?>{
