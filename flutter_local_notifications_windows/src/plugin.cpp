931d4f864a2f016e9a934e15e3a8ed7953379bdc
Levi Lesches <levilesches@gmail.com>
2024-11-19 12:36:13 -0500

    Fix WindowsPlugin.ini() on Windows MSIX
    A std::vector didn't have a starting size, which messed up the Windows APIs

--- flutter_local_notifications_windows/src/plugin.cpp
+++ flutter_local_notifications_windows/src/plugin.cpp
@@ -181,12 +181,18 @@ std::optional<bool> NativePlugin::checkIdentity() {
   if (!IsWindows8OrGreater()) return false;
   uint32_t length = 0;
   auto error = GetCurrentPackageFullName(&length, nullptr);
+<<<<<<< Updated upstream
   if (error == APPMODEL_ERROR_NO_PACKAGE) {
     return false;
   } else if (error != ERROR_INSUFFICIENT_BUFFER) {
     return std::nullopt;
   }
   std::vector<wchar_t> fullName;
+=======
+  if (error == APPMODEL_ERROR_NO_PACKAGE) return false;
+  else if (error != ERROR_INSUFFICIENT_BUFFER) return std::nullopt;
+	std::vector<wchar_t> fullName(length);
+>>>>>>> Stashed changes
   error = GetCurrentPackageFullName(&length, fullName.data());
   if (error != ERROR_SUCCESS) return std::nullopt;
   return true;
