fe1ad44a1ec4f3c500781605de91c36e60b82ca2
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2024-12-15 16:27:18 +1100

    fixed Swift Converters class

--- flutter_local_notifications/macos/flutter_local_notifications/Sources/flutter_local_notifications/Converters.swift
+++ flutter_local_notifications/macos/flutter_local_notifications/Sources/flutter_local_notifications/Converters.swift
@@ -1,27 +1,27 @@
 import UserNotifications
 
 public class Converters {
-    public static func parseNotificationCategoryOptions(_ options: [AnyHashable]?) -> UNNotificationCategoryOptions {
-        var result = Int(UNNotificationCategoryOption.none)
+    public static func parseNotificationCategoryOptions(_ options: [Any]?) -> UNNotificationCategoryOptions {
+        var result:UInt = 0
 
         for option in options ?? [] {
             guard let option = option as? NSNumber else {
                 continue
             }
-            result |= option.intValue
+            result |= option.uintValue
         }
 
         return UNNotificationCategoryOptions(rawValue: result)
     }
 
-    public static func parseNotificationActionOptions(_ options: [AnyHashable]?) -> UNNotificationActionOptions {
-        var result = Int(UNNotificationActionOption.none)
+    public static func parseNotificationActionOptions(_ options: [Any]?) -> UNNotificationActionOptions {
+        var result:UInt = 0
 
         for option in options ?? [] {
             guard let option = option as? NSNumber else {
                 continue
             }
-            result |= option.intValue
+            result |= option.uintValue
         }
 
         return UNNotificationActionOptions(rawValue: result)
