26c5b71620d25891ebf959250443115c026d69f7
Levi Lesches <levilesches@gmail.com>
2024-12-24 15:17:38 -0500

    Pre-emptively deleted Java code

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/models/styles/StyleInformation.java
+++
@@ -1,8 +0,0 @@
-package com.dexterous.flutterlocalnotifications.models.styles;
-
-import androidx.annotation.Keep;
-
-import java.io.Serializable;
-
-@Keep
-public abstract class StyleInformation implements Serializable {}
