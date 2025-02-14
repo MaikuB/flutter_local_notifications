e08a77dc5235f7f930b04a536eb48cea00d3c814
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2025-02-14 19:59:06 +1100

    added updated generated files for example app after flutter_timezone bump

--- flutter_local_notifications/example/linux/flutter/generated_plugin_registrant.cc
+++ flutter_local_notifications/example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_timezone/flutter_timezone_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_timezone_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterTimezonePlugin");
+  flutter_timezone_plugin_register_with_registrar(flutter_timezone_registrar);
 }
--- flutter_local_notifications/example/windows/flutter/generated_plugin_registrant.cc
+++ flutter_local_notifications/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_timezone/flutter_timezone_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterTimezonePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterTimezonePluginCApi"));
 }
