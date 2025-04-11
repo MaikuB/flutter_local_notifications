01418c81e282f499712bbdd1b1ce102a43ee919f
bannzai <kingkong999yhirose@gmail.com>
2025-04-11 10:19:40 +0900

    Separete first sentence

--- flutter_local_notifications/lib/src/platform_specifics/darwin/notification_details.dart
+++ flutter_local_notifications/lib/src/platform_specifics/darwin/notification_details.dart
@@ -142,6 +142,7 @@ class DarwinNotificationDetails {
   final InterruptionLevel? interruptionLevel;
 
   /// The volume for playing the critical alert sound.
+  ///
   /// The value is between 0.0 and 1.0.
   /// This is only used when the notification is configured as a critical alert.
   /// Document: https://developer.apple.com/documentation/usernotifications/unnotificationsound/criticalsoundnamed(_:withaudiovolume:)
