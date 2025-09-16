2f6eda0095ee5c1ca5179288146fdaed24ec2a45
Levi Lesches <levilesches@gmail.com>
2025-09-16 18:54:33 -0400

    Documented actions

--- flutter_local_notifications_web/lib/src/action.dart
+++ flutter_local_notifications_web/lib/src/action.dart
@@ -1,12 +1,23 @@
+/// The type of action for a web notification.
+///
+/// Note: This is not standard yet, see: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification#browser_compatibility
+/// Notably, Firefox and Safari do not support this yet.
 enum WebNotificationActionType {
+  /// This action is a button the user can press.
   button('button'),
+
+  /// This action is an input field the user can type into.
   textInput('text');
 
   const WebNotificationActionType(this.jsValue);
+
+  /// A string to pass to the JavaScript functions that indicates this type.
   final String jsValue;
 }
 
+/// A means for a user to interact with a web notification.
 class WebNotificationAction {
+  /// A const constructor.
   const WebNotificationAction({
     required this.action,
     required this.title,
@@ -14,8 +25,15 @@ class WebNotificationAction {
     this.type = WebNotificationActionType.button,
   });
 
+  /// The type of action (button by default).
   final WebNotificationActionType type;
+
+  /// A developer-facing string representing this action.
   final String action;
+
+  /// A user-facing string to be shown next to this action.
   final String title;
+
+  /// An optional icon to display next to the action.
   final Uri? icon;
 }
