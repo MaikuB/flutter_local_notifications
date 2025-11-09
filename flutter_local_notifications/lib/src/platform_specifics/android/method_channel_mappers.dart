e4bcc113ff17be90b28020d47b616e60775dc4ae
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2025-11-09 14:39:14 +1100

    Revert "Fix lints"
    This reverts commit a8b462e66bbd12ffce10341e0d825023298d1059.

--- flutter_local_notifications/lib/src/platform_specifics/android/method_channel_mappers.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/method_channel_mappers.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'enums.dart';
 import 'initialization_settings.dart';
 import 'message.dart';
@@ -52,10 +50,10 @@ extension AndroidNotificationChannelMapper on AndroidNotificationChannel {
         'enableVibration': enableVibration,
         'vibrationPattern': vibrationPattern,
         'enableLights': enableLights,
-        'ledColorAlpha': ledColor?.alpha2,
-        'ledColorRed': ledColor?.red2,
-        'ledColorGreen': ledColor?.green2,
-        'ledColorBlue': ledColor?.blue2,
+        'ledColorAlpha': ledColor?.alpha,
+        'ledColorRed': ledColor?.red,
+        'ledColorGreen': ledColor?.green,
+        'ledColorBlue': ledColor?.blue,
         'audioAttributesUsage': audioAttributesUsage.value,
         'channelAction':
             AndroidNotificationChannelAction.createIfNotExists.index,
@@ -194,10 +192,10 @@ extension AndroidNotificationDetailsMapper on AndroidNotificationDetails {
         'autoCancel': autoCancel,
         'ongoing': ongoing,
         'silent': silent,
-        'colorAlpha': color?.alpha2,
-        'colorRed': color?.red2,
-        'colorGreen': color?.green2,
-        'colorBlue': color?.blue2,
+        'colorAlpha': color?.alpha,
+        'colorRed': color?.red,
+        'colorGreen': color?.green,
+        'colorBlue': color?.blue,
         'onlyAlertOnce': onlyAlertOnce,
         'showWhen': showWhen,
         'when': when,
@@ -208,10 +206,10 @@ extension AndroidNotificationDetailsMapper on AndroidNotificationDetails {
         'progress': progress,
         'indeterminate': indeterminate,
         'enableLights': enableLights,
-        'ledColorAlpha': ledColor?.alpha2,
-        'ledColorRed': ledColor?.red2,
-        'ledColorGreen': ledColor?.green2,
-        'ledColorBlue': ledColor?.blue2,
+        'ledColorAlpha': ledColor?.alpha,
+        'ledColorRed': ledColor?.red,
+        'ledColorGreen': ledColor?.green,
+        'ledColorBlue': ledColor?.blue,
         'ledOnMs': ledOnMs,
         'ledOffMs': ledOffMs,
         'ticker': ticker,
@@ -298,10 +296,10 @@ extension AndroidNotificationDetailsMapper on AndroidNotificationDetails {
             (AndroidNotificationAction e) => <String, dynamic>{
               'id': e.id,
               'title': e.title,
-              'titleColorAlpha': e.titleColor?.alpha2,
-              'titleColorRed': e.titleColor?.red2,
-              'titleColorGreen': e.titleColor?.green2,
-              'titleColorBlue': e.titleColor?.blue2,
+              'titleColorAlpha': e.titleColor?.alpha,
+              'titleColorRed': e.titleColor?.red,
+              'titleColorGreen': e.titleColor?.green,
+              'titleColorBlue': e.titleColor?.blue,
               if (e.icon != null) ...<String, Object>{
                 'icon': e.icon!.data,
                 'iconBitmapSource': e.icon!.source.index,
@@ -330,12 +328,4 @@ extension AndroidNotificationDetailsMapper on AndroidNotificationDetails {
         'label': input.label,
         'allowedMimeType': input.allowedMimeTypes.toList(),
       };
-}
-
-extension on Color {
-  // These original getters were deprecated.
-  int get alpha2 => (a * 255.0).round();
-  int get red2 => (r * 255.0).round();
-  int get green2 => (g * 255.0).round();
-  int get blue2 => (b * 255.0).round();
 }
