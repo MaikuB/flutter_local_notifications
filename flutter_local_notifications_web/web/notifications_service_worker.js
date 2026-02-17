05658e2cb8eedd0888b76fe7eb249c114cb98c87
Gaurav <gaurav.yadav@caremonitor.com.au>
2026-02-17 11:37:08 +0530

    refactor(flutter_local_notifications_web): Remove unused IndexedDB functionality
    - Remove saveMessage() function and related IndexedDB database logic
    - Remove idb_id field from notification message object
    - Remove obsolete comments referencing IndexedDB usage
    - Simplify notification handling by eliminating dead code path

--- flutter_local_notifications_web/web/notifications_service_worker.js
+++ flutter_local_notifications_web/web/notifications_service_worker.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// Experimental function to save a notification to the IndexedDB database
-//
-// This function isn't currently used but left because it may be helpful in other
-// implementations of this plugin or custom needs for some apps.
-function saveMessage(message) {
-  let request = indexedDB.open("flutter_local_notifications", 1);
-  request.onupgradeneeded = (event) => {
-    let db = event.target.result;
-    db.createObjectStore("notifications", {keyPath: "idb_id"});
-  }
-  request.onsuccess = (event) => {
-    let db = event.target.result;
-    let transaction = db.transaction(["notifications"], "readwrite");
-    transaction.objectStore("notifications").add(message);
-  }
-}
-
 // Handles a clicked notification.
 //
 // If the site is open, sends the information to the site using postMessage().
@@ -35,11 +18,7 @@ async function _handleNotif(event) {
     payload: event.notification.data.payload,
     action: event.action,
     reply: event.reply,
-    // This is only used for saveMessage() above.
-    idb_id: "flutter_local_notifications",
   };
-  // If you need to hold onto the message:
-  // saveMessage(message)
 
   if (allClients.length == 0) {
     let url = `/?notification_id=${encodeURIComponent(message.id)}`
